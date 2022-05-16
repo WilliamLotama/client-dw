5c3c5d3592f46897410e7d6f757b48ca822c3595
william <williamkurniawan10@gmail.com>
2022-05-17 04:06:48 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,7 @@
 export const API = () => {
-  const baseUrl = "http://localhost:5000/api/v1";
+  process.env.SERVER_URL ||
+    "https://dumbmerch-will.herokuapp.com/api/v1" ||
+    "https://localhost:5000/api/v1";
 
   const executeAPI = async (endpoint, config) => {
     const response = await fetch(baseUrl + endpoint, config);
